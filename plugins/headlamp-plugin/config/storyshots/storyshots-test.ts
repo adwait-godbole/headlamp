e8c90666159df36885799e9e39bb1118cf6d8a3f
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-01-28 16:44:17 +0100

    plugins: Improve how storyshots are skipped
    Signed-off-by: Oleksandr Dubenko <oldubenko@microsoft.com>

--- plugins/headlamp-plugin/config/storyshots/storyshots-test.ts
+++ plugins/headlamp-plugin/config/storyshots/storyshots-test.ts
@@ -1,6 +1,9 @@
+import { test } from 'vitest';
+
 export function initTests() {
   console.warn(
     'Storyshots are deprecated. See more at: https://storybook.js.org/docs/writing-tests/storyshots-migration-guide/'
   );
-  expect(true).toBe(true);
+
+  test.skip('Storyshots are deprecated', () => {});
 }
